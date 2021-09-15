2085a83e04c8dc44bbddb86946e4a432be706610
Juan Velasco Mariscal <juan.velasco.mariscal@gmv.com>
2021-09-15 14:10:30 +0200

    In progress Challenge 3 Section 19

--- section19_IOStreams/Section19/Challenge_3/main.cpp
+++ section19_IOStreams/Section19/Challenge_3/main.cpp
@@ -2,13 +2,41 @@
 // Challenge 3
 // Word counter
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-using namespace std;
+
+void clean(std::string){
+    
+}
 
 int main() {
+    std::fstream in_file;
+    std::string filename {".//romeoandjuliet.txt"};
+
+    std::vector<std::string> words;
+    std::string currentWord;
     
+    in_file.open(filename);
+
+    if (in_file.is_open()){
+        while (!in_file.eof()){
+            in_file >> currentWord;
+            if (currentWord != "|"){
+                words.push_back(currentWord);
+            }
+        }
+    }else{
+        std::cout << "File not found" << std::endl;
+    }
     
-    cout << endl;
+    for (size_t i{0}; i < 20; ++i){
+            std::cout << words.at(i) << std::endl;
+    }
+
+    
+
     return 0;
 }
 
