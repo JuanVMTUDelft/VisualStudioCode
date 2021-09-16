0953cdb39df341a4cd08a5e7e250a7c736c6f4b3
Juan Velasco Mariscal <juan.velasco.mariscal@gmv.com>
2021-09-16 12:07:39 +0200

    Solved Challenge 4 and closed Challenge 3 Section 19

--- section19_IOStreams/Section19/Challenge_4/main.cpp
+++ section19_IOStreams/Section19/Challenge_4/main.cpp
@@ -2,9 +2,49 @@
 // Challenge 4
 // Copy Romeo and Juliet with line numbers
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
 int main() {
+
+    std::fstream in_file;
+    std::ofstream out_file;
+    std::string filename_in{".//romeoandjuliet"};
+    std::string filename_out {".//romeoandjuliet_num.txt"};
+    std::string line{};
+
+    in_file.open(filename_in);
+    out_file.open(filename_out);
+
+    if (in_file.is_open()){
+        std::cout << "File open" << std::endl;
+    }else{
+        std::cout << "Could not open file" << std::endl;
+    }
     
+    double numLine {1};
+    if (in_file){
+        while (std::getline(in_file, line)){
+            if (!line.empty()){
+                out_file << std::left << std::setw(7) << numLine
+                     << line << std::endl;
+                ++numLine;
+            }else{
+                out_file << line << std::endl;
+            }
+        }
+    }
+
+    if (out_file.is_open()){
+        std::cout << "File open" << std::endl;
+    }else{
+        std::cout << "Could not open file" << std::endl;
+    }
+
+    in_file.close();
+    out_file.close();
+
     return 0;
 }
 
